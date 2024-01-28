50b5eb23aade74f1e2ae51aeea494366719a4d98
itahTamar <tamar.verber@gmail.com>
2024-01-28 17:28:31 +0200

    server update

--- server/server.ts
+++ server/server.ts
@@ -11,6 +11,15 @@ app.use(express.json())
 import categoriesRoutes from "./API/Category/categoryRouters"
 app.use("/api/v1/category", categoriesRoutes)
 
+import usersRoute from "./API/users/usersRoute"
+app.use("/api/v1/users", usersRoute)
+
+import boardRoute from "./API/board/boardRoute"
+app.use("/api/v1/board", boardRoute)
+
+import pinRouters from "./API/Pin/pinRouters"
+app.use("/api/v1/pin", pinRouters)
+
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`)
 })
