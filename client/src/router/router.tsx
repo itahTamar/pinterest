842d06488292da621803d21d9fb99187bcd5ddea
itahTamar <tamar.verber@gmail.com>
2024-01-31 22:00:37 +0200

    update

--- client/src/router/router.tsx
+++ client/src/router/router.tsx
@@ -2,8 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import RLhomepage from "../view/RLhomepage";
 import Login from "../components/user/Login";
 import Register from "../components/user/Register";
-import UserPage from "../view/UserPage";
-import UserBoardPage from "../view/UserBoardPage";
+import UserPage from "../view/userPage";
+import UserBoardPage from "../view/userBoardPage";
 import { HomePage } from "../components/HomePage/HomePage";
 
 export const router = createBrowserRouter([
