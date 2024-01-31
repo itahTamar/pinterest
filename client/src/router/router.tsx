5c80ebed1637277fd63a67bc0a8bcbc4ae757d65
hadaritzhaki <127751975+hadaritzhaki@users.noreply.github.com>
2024-01-31 20:29:56 +0200

    updates

--- client/src/router/router.tsx
+++ client/src/router/router.tsx
@@ -2,9 +2,10 @@ import { createBrowserRouter } from "react-router-dom";
 import RLhomepage from "../view/RLhomepage";
 import Login from "../components/user/Login";
 import Register from "../components/user/Register";
-import UserPage from "../view/UserPage";
-import UserBoardPage from "../view/UserBoardPage";
 import { HomePage } from "../components/HomePage/HomePage";
+import { CreatePin } from "../view/CreatePin/CreatePin";
+import UserBoardPage from "../view/userBoardPage";
+import UserPage from "../view/userPage";
 
 export const router = createBrowserRouter([
     {path: "", element: <RLhomepage/>},
@@ -13,5 +14,6 @@ export const router = createBrowserRouter([
     {path: "/userPage", element: <UserPage/> },
     {path: "/userBoardPage", element: <UserBoardPage/>},
     {path: "/homePage", element: <HomePage/>},
+    {path: "/createPin", element: <CreatePin/>},
     // {path: "/pinPage", element: <PinPage/>}
 ])
