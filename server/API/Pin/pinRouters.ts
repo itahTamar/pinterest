fe101573cf5d3df60e45eb8e8149ab3c500d856c
itahTamar <tamar.verber@gmail.com>
2024-02-28 21:38:24 +0200

    fix save-pin-to-user

--- server/API/Pin/pinRouters.ts
+++ server/API/Pin/pinRouters.ts
@@ -1,20 +1,31 @@
-import express from "express"
-import { addPin, deletePin,updatePin, getPinById, getAllUserSavedPinsByUserId, getAllUserCreatedPinsByUsername, findTitleAtOtherUsersPins, findTitleAtUserSavedPinsByUserId, savePinToUserByUserId, getAllOtherUsersPinsByUsername, getPinsByCategory } from "./pinCtrl"
+import express from "express";
+import {
+  addPin,
+  deletePin,
+  updatePin,
+  getPinById,
+  getAllUserSavedPinsByUserId,
+  getAllUserCreatedPinsByUsername,
+  findTitleAtOtherUsersPinsByUsername,
+  findTitleAtUserSavedPinsByUserId,
+  savePinToUserByUserId,
+  getAllOtherUsersPinsByUsername,
+  getPinsByCategory,
+} from "./pinCtrl";
 
-const router = express.Router()
-
+const router = express.Router();
 
 router
-    .post("/:user_id", addPin)
-    .post("/favorite/:pin_id", savePinToUserByUserId)   
-    .get("/saved/:user_id", getAllUserSavedPinsByUserId) 
-    .get("/others/:username", getAllOtherUsersPinsByUsername)
-    .get("/category/:category", getPinsByCategory)
-    .get("/created/:username", getAllUserCreatedPinsByUsername)
-    .get("/onePin/:pin_id", getPinById)
-    .get("/findOther/:user_id", findTitleAtOtherUsersPins)
-    .get("/findSaved/:user_id", findTitleAtUserSavedPinsByUserId)
-    .put("/:pin_id", updatePin)  
-    .delete("/:pin_id",deletePin)
-    
-export default router
+  .post("/:user_id", addPin)
+  .post("/favorite/:pin_id", savePinToUserByUserId)
+  .get("/saved/:user_id", getAllUserSavedPinsByUserId)
+  .get("/others/:username", getAllOtherUsersPinsByUsername)
+  .get("/category/:category", getPinsByCategory)
+  .get("/created/:username", getAllUserCreatedPinsByUsername)
+  .get("/onePin/:pin_id", getPinById)
+  .get("/findOther/:username", findTitleAtOtherUsersPinsByUsername)
+  .get("/findSaved/:user_id", findTitleAtUserSavedPinsByUserId)
+  .put("/:pin_id", updatePin)
+  .delete("/:pin_id", deletePin);
+
+export default router;
